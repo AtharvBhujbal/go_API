0515ff358f908aa2cc96ac3198f5348c11e2717d
Atharv Bhujbal <126101769+AtharvBhujbal@users.noreply.github.com>
2024-09-27 22:57:40 +0530

    API service initiator

---
+++ cmd/api/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/AtharvBhujbal/go_API/internal/handlers"
+	"github.com/go-chi/chi"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	log.setReportCaller(true)
+	var r *chi.Mux = chi.NewRouter()
+	handlers.Handler()
+
+	fmt.Println("Starting GO API Service....")
+
+	fmt.Println(`
+	 ______     ______        ______     ______   __    
+	/\  ___\   /\  __ \      /\  __ \   /\  == \ /\ \   
+	\ \ \__ \  \ \ \/\ \     \ \  __ \  \ \  _-/ \ \ \  
+	 \ \_____\  \ \_____\     \ \_\ \_\  \ \_\    \ \_\ 
+	  \/_____/   \/_____/      \/_/\/_/   \/_/     \/_/ `)
+
+	err := http.ListenAndServe("localhost:8000", r)
+	if err != nil {
+		log.Error(err)
+	}
+}
