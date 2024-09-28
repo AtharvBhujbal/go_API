716a0406dbe5d33b660771644be82e4cdd300be6
Atharv Bhujbal <126101769+AtharvBhujbal@users.noreply.github.com>
2024-09-28 12:07:10 +0530

    Add database and mockdb implementation

---
+++ internal/tools/mockdb.go
@@ -0,0 +1,59 @@
+package tools
+
+import "time"
+
+type mockDB struct{}
+
+var mockLoginDetails = map[string]LoginDetails{
+	"alex": {
+		AuthToken: "123ABC",
+		Username:  "alex",
+	},
+	"john": {
+		AuthToken: "456DEF",
+		Username:  "john",
+	},
+	"marie": {
+		AuthToken: "789GHI",
+		Username:  "marie",
+	},
+}
+
+var mockCoinDetails = map[string]CoinDetails{
+	"alex": {
+		Coins:    100,
+		Username: "alex",
+	},
+	"john": {
+		Coins:    200,
+		Username: "john",
+	},
+	"marie": {
+		Coins:    300,
+		Username: "marie",
+	},
+}
+
+func (d *mockDB) GetUserLoginDetails(username string) *LoginDetails {
+	time.Sleep(1 * time.Second)
+	var clientData LoginDetails
+	clientData, ok := mockLoginDetails[username]
+	if !ok {
+		return nil
+	}
+	return &clientData
+}
+
+func (d *mockDB) GetUserCoins(username string) *CoinDetails {
+	time.Sleep(1 * time.Second)
+	var clientData = CoinDetails{}
+	clientData, ok := mockCoinDetails[username]
+	if !ok {
+		return nil
+	}
+	return &clientData
+}
+
+func (d *mockDB) SetupDatabase() error {
+	return nil
+}
